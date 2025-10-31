0ed8a9ef6ae9e681f028d793f13f45024e296e42
Rasmus Kivi <rallejansson37@gmail.com>
2025-10-31 19:14:46 +0100

    Flake fix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
-  description = "Nix package for rusty-path-of-building";
+  description = "Nix package for Rusty Path of Building";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system:
+      f (import nixpkgs { inherit system; })
+    );
   in {
-    packages.${system}.default = pkgs.callPackage ./default.nix { 
-      makeWrapper = pkgs.makeWrapper;
-    };
+    packages = forAllSystems (pkgs: {
+      default = pkgs.callPackage ./default.nix {};
+    });
+
+    # optional: allows `nix run` or `nix build`
+    defaultPackage = self.packages.x86_64-linux.default;
+
+    # optional: allows `nix develop` if you want dev shell support
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [ pkgs.cargo pkgs.rustc ];
+      };
+    });
   };
 }
